2523f878a591a3fd5c858a902db7f69af2a719ef
sergioadmin <sergioadmin@gmail>
2019-10-22 18:36:27 +0300

    To start Lesson_23

--- app.rb
+++ app.rb
@@ -13,3 +13,40 @@ end
 get '/visit' do
 	erb :visit
 end
+
+post '/visit' do
+  # user_name, phone, date_time
+  @user_name = params[:user_name]
+  @phone = params[:phone]
+  @date_time = params[:date_time]
+  @baber = params[:baber]
+
+  @title = "Thank you!"
+  @message = "Уважаемый #{@user_name}, мы ждём вас #{@date_time} у выбранного парикмахера #{@baber}."
+
+  # запишем в файл то, что ввёл клиент
+  f = File.open './public/users.txt', 'a'
+  f.write "User: #{@user_name}, phone: #{@phone}, date and time: #{@date_time}. Baber: #{@baber}.\n"
+  f.close
+
+  erb :message
+end
+
+get '/admin' do
+	erb :admin
+end
+
+post '/admin' do
+  @login = params[:login]
+  @password = params[:password]
+
+  # проверим логин и пароль, и пускаем внутрь или нет:
+  if @login == 'admin' && @password == '12345'
+    @file = File.open("./public/users.txt","r")
+    erb :watch_result
+    # @file.close - должно быть, но тогда не работает. указал в erb
+  else
+    @report = '<p>Доступ запрещён! Неправильный логин или пароль.</p>'
+    erb :admin
+  end
+end
